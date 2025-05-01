6350e63b23661f4483c2c9d04c445f022513c8c9
Orlando Fernand <orlando.fernand18@stjohns.edu>
2025-05-01 07:04:24 -0400

    Added files

---
+++ Public/AreaOfRectangle.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate the area of a rectangle
+
+def area_of_rectangle(length, width):
+    return length * width
+
+print(area_of_rectangle(5, 3))
---
+++ Public/ConvertStringToInteger.py
@@ -0,0 +1,5 @@
+# Write a Python code to convert a string to an integer
+
+str_num = "12345"
+int_num = int(str_num)
+print(int_num)
